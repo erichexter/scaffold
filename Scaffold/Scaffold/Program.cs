f4aa950bb0da1cfe8fc32dc65e44a81b5eab712f
Eric Hexter <eric.hexter@gmail.com>
2015-03-26 07:03:58 -0500

    initial commit

---
+++ Scaffold/Scaffold/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace Scaffold
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var files = new List<string>();
+            string scaffoldName = args[0];
+            string rootFolder = args[1];
+            string modelJson = args[2];
+            string projectNamespace = args[3];
+
+            var scaffoldPath = "Scaffolds\\"+scaffoldName+"\\";
+
+            var configurationJson = File.OpenText(scaffoldPath+"scaffold.json").ReadToEnd();
+            var configuration = JsonConvert.DeserializeObject<ScaffoldConfiguration>(configurationJson);
+
+            var model = new ScaffoldModel(){
+                ProjectNamespace = projectNamespace,
+                Model = new ModelType() { Name = modelJson}
+            };
+
+            foreach (var templateConfiguration in configuration.templates)
+            {
+                var template = File.OpenText(scaffoldPath+templateConfiguration.name).ReadToEnd();
+
+                string result = Engine.Razor.RunCompile(template, templateConfiguration.name, typeof(ScaffoldModel), model);
+
+                string destinationPath = Engine.Razor.RunCompile(templateConfiguration.destination, templateConfiguration.name+"Path", typeof(string), model.Model.Name);
+
+                var outputPath = Path.Combine(rootFolder, destinationPath);
+
+                Directory.CreateDirectory( Path.GetDirectoryName( outputPath));
+                File.WriteAllText(outputPath,result);
+                files.Add(Path.GetFullPath(  outputPath));
+            }
+            //return full file paths. vs2013 can use this to add files to projects.
+            Console.Write(JsonConvert.SerializeObject(files));
+            
+        }
+    }
+} 
---
+++ Scaffold/Scaffold/ScaffoldModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaffold
+{
+    public class ScaffoldModel
+    {
+        public string ProjectNamespace { get; set; }
+        public ModelType Model { get; set; }
+    }
+
+    public class ModelType
+    {
+        private readonly List<ModelProperty> _properties = new List<ModelProperty>();
+        public string Name;
+        public string Namespace;
+        private List<ModelProperty> _methods = new List<ModelProperty>();
+        private List<string> _bases = new List<string>();
+
+        public List<ModelProperty> Properties
+        {
+            get { return _properties; }
+        }
+
+        public List<ModelProperty> Methods
+        {
+            get { return _methods; }
+        }
+
+        public List<string> Bases
+        {
+            get
+            {
+                return _bases;
+            }
+        }
+    }
+    public class ModelProperty
+    {
+        public ModelType ModelType;
+        public string Name;
+    }
+}
+
+public class ScaffoldConfiguration
+{
+    public string name { get; set; }
+    public Template[] templates { get; set; }
+}
+
+public class Template
+{
+    public string name { get; set; }
+    public string destination { get; set; }
+}
